54a81444eab22174a4fca690bb503f407698da39
lizhihui0215 <lizhihui0215@gmail.com>
2014-12-04 21:25:59 +0800

    CocoPods

---
+++ HelloCocoPods/HelloCocoPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloCocoPods
+//
+//  Created by lizhihui on 12/4/14.
+//  Copyright (c) 2014 ___LZSoftware___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloCocoPods/HelloCocoPods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloCocoPods
+//
+//  Created by lizhihui on 12/4/14.
+//  Copyright (c) 2014 ___LZSoftware___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
